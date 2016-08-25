8b23debd28f255672843cd2c42e6fdfca6a107f0
Hack Reactor Students <accounts@hackreactor.com>
2016-08-25 16:02:42 -0700

    Implement mongoDB

--- app/collections/links.js
+++ app/collections/links.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 
 var urlsSchema = new mongoose.Schema({
   url: String,
@@ -9,5 +10,11 @@ var urlsSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now }
 });
 
+urlsSchema.pre('save', function(done) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  done();
+});
 
 module.exports = urlsSchema;
--- app/collections/users.js
+++ app/collections/users.js
@@ -1,9 +1,37 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
 
 var usersSchema = new mongoose.Schema({
   username: String,
   password: String,
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
+
 });
 
+usersSchema.pre('save', function (done) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      done();
+    });
+});
+
+usersSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    console.log(err); //thisworks?
+    callback(isMatch);
+  });
+};
+
+// usersSchema.methods.hashPassword = function() {
+// };
+
 module.exports = usersSchema;
+
+// var cipher = Promise.promisify(bcrypt.hash);
+//   return cipher(this.get('password'), null, null).bind(this)
+//     .then(function(hash) {
+//       this.set('password', hash);
+//     });
