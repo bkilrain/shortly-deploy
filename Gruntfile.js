267c8f6ed52078c4e4a1f09f0a9c1e447f88064b
Hack Reactor Students <accounts@hackreactor.com>
2016-08-24 17:13:31 -0700

    testing2

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,7 @@ module.exports = function(grunt) {
 
     shell: {
       prodServer: {
+        command: 'git push live master'
       }
     },
   });
@@ -81,14 +82,18 @@ module.exports = function(grunt) {
 
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
+      console.log('prod');
       // add your production server task here
+      grunt.task.run(['shell:prodServer']);
     } else {
+      console.log(' no prod');
       grunt.task.run([ 'server-dev' ]);
     }
   });
 
   grunt.registerTask('deploy', [
     // add your deploy tasks here
+    'upload'
   ]);
 
 
